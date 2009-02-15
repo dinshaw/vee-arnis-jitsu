382e8e75748fd81f64cc66aeefd572334d47a236
Dinshaw Gobhai <info@dinshawdesign.com>
2009-02-15 13:14:57 -0500

    replace admin filter

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,11 @@ class User < ActiveRecord::Base
 
   has_and_belongs_to_many :roles
 
-  validates_format_of       :first_name,     :with => RE_NAME_OK,  :message => MSG_NAME_BAD
-  validates_length_of       :first_name,     :maximum => 100
-  validates_format_of       :last_name,     :with => RE_NAME_OK,  :message => MSG_NAME_BAD
-  validates_length_of       :last_name,     :maximum => 100
-  
+  # validates_format_of       :first_name,     :with => RE_NAME_OK,  :message => MSG_NAME_BAD
+  # validates_length_of       :first_name,     :maximum => 100
+  # validates_format_of       :last_name,     :with => RE_NAME_OK,  :message => MSG_NAME_BAD
+  # validates_length_of       :last_name,     :maximum => 100
+  # 
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
   validates_uniqueness_of   :email,    :case_sensitive => false
